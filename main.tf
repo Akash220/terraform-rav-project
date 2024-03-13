a5b946b85a7f405f178dc5bee9d0a0f3a21eb3ae
akashr <akashr@presidio.com>
2024-03-13 19:19:20 +0530

    adding ec2 instances

---
+++ main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.39.1"
+    }
+  }
+  backend "S3" {
+    bucket = "terra-bucket-96"
+    key = "tamilcloud/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+provider "aws" {
+    region = "us-east-1"
+}
+
+resource "aws_instance" "my-ec2" {
+    ami = "ami-0f403e3180720dd7e"
+    instance_type = "t2.micro"
+}
+
+
+
